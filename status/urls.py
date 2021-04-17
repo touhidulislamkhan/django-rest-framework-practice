07f535bb45c6ee32a3b5ba58f616a7abeddd860f
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-18 02:48:11 +0600

    using viewsets and router

--- status/urls.py
+++ status/urls.py
@@ -1,18 +1,24 @@
 from django.urls import path
 from status import views
 
+# Router
+from rest_framework import routers
+
 # status/ => List, Create => GET, POST
 # status/<id>/ => Details, Delete, Update => GET, DELETE, PUT/PATCH
 
 
+router = routers.DefaultRouter()
+router.register(r"status", views.StatusViewset, basename='status')
+
 urlpatterns = [
     # path('status/', views.StatusAPIView.as_view()),
-    path('status/', views.StatusListCreateView.as_view()),
-    path('status/<id>/', views.StatusDetailUpdateDeleteView.as_view()),
+    # path('status/', views.StatusListCreateView.as_view()),
+    # path('status/<id>/', views.StatusDetailUpdateDeleteView.as_view()),
     # path('status/<id>/', views.StatusDetailAPIView.as_view()),
     # path('status/list/', views.StatusListAPIView.as_view()),
     # path('status/create/', views.StatusCreateAPIView.as_view()),
     # path('status/detail/<id>/', views.StatusDetailAPIView.as_view()),
     # path('status/update/<id>/', views.StatusUpdateAPIView.as_view()),
     # path('status/delete/<id>/', views.StatusDeleteAPIView.as_view()),
-]
+] + router.urls
--- status/views.py
+++ status/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 
 from rest_framework import generics, mixins
 
-# Parsers
-from rest_framework import parsers
+# Parsers, Viewsets
+from rest_framework import parsers, viewsets
 
 # Create your views here.
 
@@ -21,13 +21,13 @@ from rest_framework import parsers
 #         return Response(status_serializer.data)
 
 
-class StatusListCreateView(generics.ListCreateAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+# class StatusListCreateView(generics.ListCreateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
 
-    # def post(self, request, *args, **kwargs):
-    #     return self.create(request, *args, **kwargs)
+# def post(self, request, *args, **kwargs):
+#     return self.create(request, *args, **kwargs)
 
 
 # class StatusListAPIView(generics.ListAPIView):
@@ -40,31 +40,31 @@ class StatusListCreateView(generics.ListCreateAPIView):
 #     serializer_class = StatusSerializer
 
 
-class StatusDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = "id"
-    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+# class StatusDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = "id"
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
 
 # class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Status.objects.all()
 #     serializer_class = StatusSerializer
 #     lookup_field = "id"
 
-    # def get_object(self, *args, **kwargs):
-    #     kwargs = self.kwargs
-    #     kw_id = kwargs.get('id')
+# def get_object(self, *args, **kwargs):
+#     kwargs = self.kwargs
+#     kw_id = kwargs.get('id')
 
-    #     return Status.objects.get(id=kw_id)
+#     return Status.objects.get(id=kw_id)
 
-    # def put(self, request, *args, **kwargs):
-    #     return self.update(request, *args, **kwargs)
+# def put(self, request, *args, **kwargs):
+#     return self.update(request, *args, **kwargs)
 
-    # def patch(self, request, *args, **kwargs):
-    #     return self.partial_update(request, *args, **kwargs)
+# def patch(self, request, *args, **kwargs):
+#     return self.partial_update(request, *args, **kwargs)
 
-    # def delete(self, request, *args, **kwargs):
-    #     return self.destroy(request, *args, **kwargs)
+# def delete(self, request, *args, **kwargs):
+#     return self.destroy(request, *args, **kwargs)
 
 
 # class StatusUpdateAPIView(generics.UpdateAPIView):
@@ -77,3 +77,9 @@ class StatusDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Status.objects.all()
 #     serializer_class = StatusSerializer
 #     lookup_field = "id"
+
+
+class StatusViewset(viewsets.ModelViewSet):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
